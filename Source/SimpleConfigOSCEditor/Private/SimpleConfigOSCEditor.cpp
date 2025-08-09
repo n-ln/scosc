11f00e98e68e433044d02629613a272525a5e854
n-ln <153449615+n-ln@users.noreply.github.com>
2025-08-09 01:51:07 -0400

    Rebuild editor tab

--- Source/SimpleConfigOSCEditor/Private/SimpleConfigOSCEditor.cpp
+++ Source/SimpleConfigOSCEditor/Private/SimpleConfigOSCEditor.cpp
@@ -73,107 +73,246 @@ TSharedRef<SDockTab> FSimpleConfigOSCEditorModule::OnSpawnPluginTab(const FSpawn
 	//.TabRole(ETabRole::NomadTab)
 	//.Label(LOCTEXT("DockableTabTitle", "SCOSC Editor"))
 	[
-		SNew(SBorder)
-		.HAlign(HAlign_Fill)
-		.VAlign(VAlign_Fill)
-		.BorderImage(FStyleDefaults::GetNoBrush())
+		SNew(SVerticalBox)
+		// Title
+		+ SVerticalBox::Slot()
+		.AutoHeight()
+		.Padding(4.f)
+		[
+			SNew(STextBlock)
+			.Text(LOCTEXT("EditorTitle", "SCOSC Editor"))
+			.Font(FCoreStyle::GetDefaultFontStyle("Bold", 14))
+		]
+		+ SVerticalBox::Slot()
+		.AutoHeight()
+		[
+			SNew(SSeparator)
+		]
+		// OSC Server
+		+ SVerticalBox::Slot()
+		.FillHeight(4.f)
+		.Padding(4.f)
 		[
 			SNew(SVerticalBox)
-			// Title Bar
 			+ SVerticalBox::Slot()
-			.HAlign(HAlign_Fill)
-			.VAlign(VAlign_Fill)
-			.FillHeight(1.f)
-			.Padding(4.f)
+			.AutoHeight()
 			[
-				SNew(SHorizontalBox)
-				+ SHorizontalBox::Slot()
-				.AutoWidth()
+				SNew(SButton)
+				.Text(FText::FromString("Toggle OSC Server"))
+				.HAlign(HAlign_Center)
 				.VAlign(VAlign_Center)
-				[
-					SNew(STextBlock)
-					.Text(LOCTEXT("EditorTitle", "SOCSC Editor"))
-					// Set style for title text
-					.Font(FCoreStyle::GetDefaultFontStyle("Bold", 14))
-				]
 			]
-			// OSC Server
 			+ SVerticalBox::Slot()
-			.HAlign(HAlign_Fill)
-			.VAlign(VAlign_Fill)
-			.FillHeight(4.f)
-			.Padding(4.f)
-			[
-				SNew(SVerticalBox)
-				+ SVerticalBox::Slot()
-				.MaxHeight(40.f)
-				[
-					SNew(SButton)
-					.Text(FText::FromString("Toggle OSC Server"))
-					.HAlign(HAlign_Center)
-					.VAlign(VAlign_Center)
-				]
-				+ SVerticalBox::Slot()
-				//.FillHeight()
-				[
-					SNew(SHorizontalBox)
-					+ SHorizontalBox::Slot()
-					[
-						SNew(STextBlock)
-						.Text(LOCTEXT("EditorAddrList", "OSC Address List"))
-					]
-					+ SHorizontalBox::Slot()
-					[
-						SNew(STextBlock)
-						.Text(LOCTEXT("EditorServerDetail", "OSC Server Details"))
-					]
-				]
-			]
-			// OSC Client
-			+ SVerticalBox::Slot()
-			.HAlign(HAlign_Fill)
-			.VAlign(VAlign_Fill)
-			.FillHeight(4.f)
-			.Padding(4.f)
-			[
-				SNew(SVerticalBox)
-				+ SVerticalBox::Slot()
-				.MaxHeight(40.f)
-				[
-					SNew(SButton)
-					.Text(FText::FromString("Toggle OSC Client"))
-					.HAlign(HAlign_Center)
-					.VAlign(VAlign_Center)
-				]
-				+ SVerticalBox::Slot()
-				[
-					SNew(SHorizontalBox)
-					+ SHorizontalBox::Slot()
-					[
-						SNew(STextBlock)
-						.Text(LOCTEXT("EditorDestList", "OSC Destination List"))
-					]
-					+ SHorizontalBox::Slot()
-					[
-						SNew(STextBlock)
-						.Text(LOCTEXT("EditorClientDetail", "OSC Client Details"))
-					]
-				]
-			]
-			// OSC Status
-			+ SVerticalBox::Slot()
-			.HAlign(HAlign_Fill)
-			.VAlign(VAlign_Fill)
 			.FillHeight(1.f)
-			.Padding(4.f)
+			.Padding(0.f, 8.f, 0.f, 0.f)
 			[
-				SNew(SHorizontalBox)
-				+ SHorizontalBox::Slot()
+				SNew(SSplitter)
+				.Orientation(Orient_Horizontal)
+				+ SSplitter::Slot()
+				.Value(0.25f)
 				[
-					SNew(STextBlock)
-					.Text(LOCTEXT("EditorStatus", "OSC Status"))
+					SNew(SBorder)
+					.BorderImage(FStyleDefaults::GetNoBrush())
+					.Padding(4.f)
+					[
+						SNew(SVerticalBox)
+						+ SVerticalBox::Slot()
+						.AutoHeight()
+						.Padding(0.f, 0.f, 0.f, 4.f)
+						[
+							SNew(STextBlock)
+							.Text(LOCTEXT("EditorAddrList", "OSC Address List"))
+							.Font(FCoreStyle::GetDefaultFontStyle("Bold", 10))
+						]
+						+ SVerticalBox::Slot()
+						.FillHeight(1.f)
+						[
+							SNew(SListView<TSharedPtr<FString>>)
+							.SelectionMode(ESelectionMode::Single)
+							.ListItemsSource(&OSCAddressList)
+						]
+					]
+				]
+				+ SSplitter::Slot()
+				.Value(0.75f)
+				[
+					SNew(SBorder)
+					.BorderImage(FStyleDefaults::GetNoBrush())
+					.Padding(4.f)
+					[
+						SNew(SVerticalBox)
+						+ SVerticalBox::Slot()
+						.AutoHeight()
+						.Padding(0.f, 0.f, 0.f, 4.f)
+						[
+							SNew(STextBlock)
+							.Text(LOCTEXT("EditorServerDetail", "OSC Server Details"))
+							.Font(FCoreStyle::GetDefaultFontStyle("Bold", 10))
+						]
+						+ SVerticalBox::Slot()
+						.FillHeight(1.f)
+						[
+							SNew(SScrollBox)
+							+ SScrollBox::Slot()
+							[
+								SNew(SVerticalBox)
+								+ SVerticalBox::Slot()
+								.AutoHeight()
+								.Padding(0.f, 4.f)
+								[
+									SNew(SHorizontalBox)
+									+ SHorizontalBox::Slot()
+									.AutoWidth()
+									.VAlign(VAlign_Center)
+									.Padding(0.f, 0.f, 8.f, 0.f)
+									[
+										SNew(STextBlock)
+										.Text(LOCTEXT("ServerPort", "Port:"))
+									]
+									+ SHorizontalBox::Slot()
+									.FillWidth(1.f)
+									[
+										SNew(SEditableTextBox)
+										.Text(FText::FromString("8000"))
+									]
+								]
+							]
+						]
+					]
 				]
 			]
+		]
+		+ SVerticalBox::Slot()
+		.AutoHeight()
+		[
+			SNew(SSeparator)
+		]
+		// OSC Client
+		+ SVerticalBox::Slot()
+		.FillHeight(4.f)
+		.Padding(4.f)
+		[
+			SNew(SVerticalBox)
+			+ SVerticalBox::Slot()
+			.AutoHeight()
+			[
+				SNew(SButton)
+				.Text(FText::FromString("Toggle OSC Client"))
+				.HAlign(HAlign_Center)
+				.VAlign(VAlign_Center)
+			]
+			+ SVerticalBox::Slot()
+			.FillHeight(1.f)
+			.Padding(0.f, 8.f, 0.f, 0.f)
+			[
+				SNew(SSplitter)
+				.Orientation(Orient_Horizontal)
+				+ SSplitter::Slot()
+				.Value(0.25f)
+				[
+					SNew(SBorder)
+					.BorderImage(FStyleDefaults::GetNoBrush())
+					.Padding(4.f)
+					[
+						SNew(SVerticalBox)
+						+ SVerticalBox::Slot()
+						.AutoHeight()
+						.Padding(0.f, 0.f, 0.f, 4.f)
+						[
+							SNew(STextBlock)
+							.Text(LOCTEXT("EditorDestList", "OSC Destination List"))
+							.Font(FCoreStyle::GetDefaultFontStyle("Bold", 10))
+						]
+						+ SVerticalBox::Slot()
+						.FillHeight(1.f)
+						[
+							SNew(SListView<TSharedPtr<FString>>)
+							.SelectionMode(ESelectionMode::Single)
+							.ListItemsSource(&OSCDestinationList)
+						]
+					]
+				]
+				+ SSplitter::Slot()
+				.Value(0.75f)
+				[
+					SNew(SBorder)
+					.BorderImage(FStyleDefaults::GetNoBrush())
+					.Padding(4.f)
+					[
+						SNew(SVerticalBox)
+						+ SVerticalBox::Slot()
+						.AutoHeight()
+						.Padding(0.f, 0.f, 0.f, 4.f)
+						[
+							SNew(STextBlock)
+							.Text(LOCTEXT("EditorClientDetail", "OSC Client Details"))
+							.Font(FCoreStyle::GetDefaultFontStyle("Bold", 10))
+						]
+						+ SVerticalBox::Slot()
+						.FillHeight(1.f)
+						[
+							SNew(SScrollBox)
+							+ SScrollBox::Slot()
+							[
+								SNew(SVerticalBox)
+								+ SVerticalBox::Slot()
+								.AutoHeight()
+								.Padding(0.f, 4.f)
+								[
+									SNew(SHorizontalBox)
+									+ SHorizontalBox::Slot()
+									.AutoWidth()
+									.VAlign(VAlign_Center)
+									.Padding(0.f, 0.f, 8.f, 0.f)
+									[
+										SNew(STextBlock)
+										.Text(LOCTEXT("ClientAddress", "Address:"))
+									]
+									+ SHorizontalBox::Slot()
+									.FillWidth(1.f)
+									[
+										SNew(SEditableTextBox)
+										.Text(FText::FromString("127.0.0.1"))
+									]
+								]
+								+ SVerticalBox::Slot()
+								.AutoHeight()
+								.Padding(0.f, 4.f)
+								[
+									SNew(SHorizontalBox)
+									+ SHorizontalBox::Slot()
+									.AutoWidth()
+									.VAlign(VAlign_Center)
+									.Padding(0.f, 0.f, 8.f, 0.f)
+									[
+										SNew(STextBlock)
+										.Text(LOCTEXT("ClientPort", "Port:"))
+									]
+									+ SHorizontalBox::Slot()
+									.FillWidth(1.f)
+									[
+										SNew(SEditableTextBox)
+										.Text(FText::FromString("9000"))
+									]
+								]
+							]
+						]
+					]
+				]
+			]
+		]
+		+ SVerticalBox::Slot()
+		.AutoHeight()
+		[
+			SNew(SSeparator)
+		]
+		// OSC Status
+		+ SVerticalBox::Slot()
+		.AutoHeight()
+		.Padding(4.f)
+		[
+			SNew(STextBlock)
+			.Text(LOCTEXT("EditorStatus", "OSC Status"))
 		]
 	];
 }
--- Source/SimpleConfigOSCEditor/SimpleConfigOSCEditor.Build.cs
+++ Source/SimpleConfigOSCEditor/SimpleConfigOSCEditor.Build.cs
@@ -22,6 +22,7 @@ public class SimpleConfigOSCEditor : ModuleRules
                 "SlateCore",
                 "UnrealEd",
                 "ToolMenus",
+                "InputCore",
             }
         );
     }

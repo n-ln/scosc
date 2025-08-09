b1f87b9ea9d36f43f9100c89b3c5a098a00e5f58
n-ln <153449615+n-ln@users.noreply.github.com>
2025-08-09 17:07:12 -0400

    Refactor editor UI into widget class

---
+++ Source/SimpleConfigOSCEditor/Public/Widgets/SCOSCEditorWidget.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/SCompoundWidget.h"
+#include "Widgets/DeclarativeSyntaxSupport.h"
+
+class SSCOSCEditorWidget : public SCompoundWidget
+{
+public:
+	SLATE_BEGIN_ARGS(SSCOSCEditorWidget)
+		{}
+		SLATE_ARGUMENT(TSharedPtr<TArray<TSharedPtr<FString>>>, OSCAddressList)
+		SLATE_ARGUMENT(TSharedPtr<TArray<TSharedPtr<FString>>>, OSCDestinationList)
+	SLATE_END_ARGS()
+
+	void Construct(const FArguments& InArgs);
+
+private:
+	// Data sources from module
+	TSharedPtr<TArray<TSharedPtr<FString>>> OSCAddressList;
+	TSharedPtr<TArray<TSharedPtr<FString>>> OSCDestinationList;
+};
